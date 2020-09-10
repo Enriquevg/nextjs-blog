c9b886782e9e2f9b81c70823414d3e298b605ab0
Enrique Velarde <enrique@vg.local>
2020-09-10 16:49:41 -0500

    pwa

--- components/layout.js
+++ components/layout.js
@@ -10,7 +10,15 @@ export default function Layout({ children, home }) {
     return (
         <div className={styles.container}>
             <Head>
+                <meta charSet='utf-8'/>
+                <meta http-equiv='X-UA-Compatible' content='IE=edge'/>
+                <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'/>
+
+                <link rel="manifest" href="/manifest.json"/>
                 <link rel="icon" href="/favicon.ico" />
+                <link href='/icons/16.png' rel='icon' type='image/png' sizes='16x16' />
+                <link href='/icons/32.png' rel='icon' type='image/png' sizes='32x32' />
+                <meta name='theme-color' content='#317EFB' />
                 <meta
                     name="description"
                     content="Learn how to build a personal website using Next.js"
